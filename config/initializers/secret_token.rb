30c765602e3ccc6d7a1ce1b49ae7fdaec0b7a649
Costi <cgavrilescu@enovafinancial.com>
2012-02-20 18:55:56 -0600

    Creation of the rails 3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BookrRails3::Application.config.secret_token = 'a2e485da6c966eaedacac2f1231795d84c297fc3047542cd95888e3c692920208bac89f4e55df02a43e057ef90bb25001a4df0d1943d194433faebbc2b620d69'
